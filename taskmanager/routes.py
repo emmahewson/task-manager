88465447ee474adbacb865b6878ae4e05f7d357a
emmahewson <emhewson@hotmail.com>
2023-04-05 16:14:57 +0000

    connect add category button to database

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -6,3 +6,22 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+# This does 2 things
+# The default method is "GET" - this loads the page as above
+# When user clicks button the method is "POST"
+# In this case the user's category is added to the database
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
