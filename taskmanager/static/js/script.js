a0b5f165cb92fa8e64bdd0a335339af99dc9a678
emmahewson <emhewson@hotmail.com>
2023-04-06 15:19:40 +0000

    add task CRUD functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization
-    var sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  // sidenav initialization
+  var sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  // datepicker initialization
+  var datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {
+      done: "Select"
+    },
   });
+
+  // Select Initialization
+  var selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+
+  // Collapsible Initialization
+  var collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
+
+  // modal initialization
+  let modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+});
